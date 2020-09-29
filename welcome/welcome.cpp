6bd4d92635edc085bea7c14905fc18babd76eebd
Houche-Lin <71763829+Houche-Lin@users.noreply.github.com>
2020-09-29 17:38:10 +0800

    Add files via upload

---
+++ welcome/welcome.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	if (argc < 3 || argc >3) {
+		cout << "usage" << endl;
+		cout << "    " << argv[0] << "user" << "host" << endl;
+	}
+	else {
+		cout << "Hello" << argv[1];
+		cout << argv[3] << "very much" << argv[0];
+		cout << "Welcome to the CLI's world." << endl;
+	}
+}
+
