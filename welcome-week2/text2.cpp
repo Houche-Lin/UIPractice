6c004435c4c5e2a38a3a5e1d6a9a9dd01dd63400
Houche-Lin <71763829+Houche-Lin@users.noreply.github.com>
2020-10-05 19:03:41 +0800

    Add files via upload

---
+++ welcome-week2/text2.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+
+using namespace std;
+
+void usage(const string& cmd)	
+	{
+		string opt[]={
+			" -h host      the host name.\n"
+			" -v visitor   the visitor name.\n"
+		};
+		cout << "Usage:" << endl;
+		cout << "  " << "welcome" << " -h host -v visitor\n";
+		for(int i=0; i<(sizeof(opt))/(sizeof(string)); i++)
+		{
+			cout << opt[i];
+		}
+		exit(-1);
+	}
+void welcome(const string& host, const string& visitor) 
+	{
+		cout<<"Hello "<<visitor<<".\n";
+		cout<<"This is " << host<<"'s home, Welcome."<<endl;    
+	}
+int main(int argc, char* argv[]) 
+	{
+		string host;
+		string visitor;
+		if (argc != 6)
+		{
+			usage(argv[0]);
+		}
+		for(int i=2; i<argc; i=i+2)
+		{
+			string arg =argv[i];
+			if(arg == "-h")
+			{
+				host = argv[i + 1];		
+			}
+			else if(arg == "-v")
+			{
+				visitor = argv[i + 1];
+			}
+			else
+			{
+				usage(argv[0]);
+			}
+	}
+		welcome(host, visitor);
+		return 0;
+}
